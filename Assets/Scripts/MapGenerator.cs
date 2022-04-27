fd23797afe8e23f5b0eb6069a1eb9c235c1b7847
freddiecooper <72386496+freddiecooper@users.noreply.github.com>
2022-04-27 16:02:14 +0100

    oaidgh

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,26 +4,84 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+
+    public enum DrawMode {NoiseMap, ColourMap};
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
-    public float noiseScale;
+    public float scale;
+
+    public bool autoUpdate;
+
+    public int octaves;
+    public float persistance;
+    public float lacunarity;
+
+    public int seed;
+    public Vector2 offset;
+
+    public TerrainType[] regions;
 
     public void GenerateMap() 
     {
-        float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap (mapWidth, mapHeight, seed, scale, octaves, persistance, lacunarity, offset);
+
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for(int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap [x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height)
+                    {
+                        colourMap[y * mapWidth + x] = regions [i].colour;
+                        break;
+                    }
+                }
+            }
+        }
 
         MapDisplay display = FindObjectOfType<MapDisplay> ();
-        display.DrawNoiseMap (noiseMap);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture (TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else if (drawMode == DrawMode.ColourMap)
+        {
+            display.DrawTexture (TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+        //display.DrawNoiseMap (noiseMap);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnValidate()
     {
-        
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
     }
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color colour;
 }
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -5,22 +5,70 @@ using UnityEngine;
 public static class Noise
 {
 
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, float scale)
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset)
     {
-        float[,] noiseMap = new float [mapWidth,mapHeight];
+        float[,] noiseMap = new float [mapWidth + 1,mapHeight + 1];
+
+        System.Random prng = new System.Random (seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+            float offsetX = prng.Next(-100000, 100000) + offset.x;
+            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            octaveOffsets [i] = new Vector2 (offsetX, offsetY);
+        }
+
+        if(scale <= 0)
+        {
+            scale = 0.0001f;
+        }
+
+        float maxNoiseHeight = float.MinValue;
+        float minNoiseHeight = float.MaxValue;
+
+        float halfWidth = mapWidth / 2f;
+        float halfHieght = mapHeight / 2f;
+
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapHeight; x++)
+            {
+
+                float amplitude = 1;
+                float frequency = 1;
+                float noiseHieght = 0;
+
+                for (int i = 0; i < octaves; i++)
+                {
+                    float sampleX = (x-halfWidth) / scale * frequency + octaveOffsets[i].x;
+                    float sampleY = (y-halfHieght) / scale * frequency + octaveOffsets[i].y;
+
+                    float perlinValue = Mathf.PerlinNoise (sampleX, sampleY) * 2 - 1;
+                    noiseHieght += perlinValue * amplitude;
+
+                    amplitude *= persistance;
+                    frequency *= lacunarity;
+                }
+
+                if (noiseHieght > maxNoiseHeight)
+                {
+                    maxNoiseHeight = noiseHieght;
+                }
+                else if (noiseHieght < minNoiseHeight)
+                {
+                    minNoiseHeight = noiseHieght;
+                }
+                noiseMap [x, y] = noiseHieght;
+            }
+        }
 
         for (int y = 0; y < mapHeight; y++)
         {
             for (int x = 0; x < mapHeight; x ++)
             {
-                float sampleX = x / scale;
-                float sampleY = y / scale;
-
-                float perlinValue = Mathf.PerlinNoise (sampleX, sampleY);
-                noiseMap [x, y] = perlinValue;
+                noiseMap [x, y] = Mathf.InverseLerp (minNoiseHeight, maxNoiseHeight, noiseMap [x, y]);
             }
         }
-
         return noiseMap;
     }
 
